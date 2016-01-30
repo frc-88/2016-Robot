4f533839e5340695094206ecb866813b879b46ab
tj2 <navtges@gmail.com>
2016-01-30 15:20:37 -0500

    Paired the masters and slaves in order to communicate to one another. Set the default command as DriveWithControllerSimple.

--- 2016-Robot/src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ 2016-Robot/src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -1,9 +1,11 @@
 package org.usfirst.frc.team88.robot.subsystems;
 
 import org.usfirst.frc.team88.robot.RobotMap;
+import org.usfirst.frc.team88.robot.commands.DriveWithControllerSimple;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *	Author:
@@ -20,23 +22,47 @@ public class Drive extends Subsystem {
 		
 		// left side drive controllers
 		lTalonMaster = new CANTalon(RobotMap.driveLeftMaster);
+		lTalonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
+
 		lTalonSlave = new CANTalon(RobotMap.driveLeftSlave);
+		lTalonSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
+		lTalonSlave.set(lTalonMaster.getDeviceID());
+
 		
 		// right side drive controllers
 		rTalonMaster = new CANTalon(RobotMap.driveRightMaster);
-		rTalonSlave = new CANTalon(RobotMap.driveRightSlave);
+		rTalonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
 
+		rTalonSlave = new CANTalon(RobotMap.driveRightSlave);
+		rTalonSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
+		rTalonSlave.set(rTalonMaster.getDeviceID());
+
+	}
+	
+	private void updateSmartDashboard() {
+		SmartDashboard.putNumber("Left Encoder: ", lTalonMaster.getEncPosition());
+		SmartDashboard.putNumber("Left Encoder Quad A: ", lTalonMaster.getPinStateQuadA());
+		SmartDashboard.putNumber("Left Encoder Quad B: ", lTalonMaster.getPinStateQuadB());
+		SmartDashboard.putNumber("Left Encoder Quad Idx: ", lTalonMaster.getPinStateQuadIdx());
+		
+		SmartDashboard.putNumber("Left Master Voltage: ", lTalonMaster.getOutputVoltage());
+		SmartDashboard.putNumber("Left Master Current: ", lTalonMaster.getOutputCurrent());
+		SmartDashboard.putNumber("Left Slave Voltage: ", lTalonSlave.getOutputVoltage());
+		SmartDashboard.putNumber("Left Slave Current: ", lTalonSlave.getOutputCurrent());
+
+		SmartDashboard.putNumber("Right Encoder: ", rTalonMaster.getEncPosition());
+		SmartDashboard.putNumber("Right Master Voltage: ", rTalonMaster.getOutputVoltage());
+		SmartDashboard.putNumber("Right Master Current: ", rTalonMaster.getOutputCurrent());
+		SmartDashboard.putNumber("Right Slave Voltage: ", rTalonSlave.getOutputVoltage());
+		SmartDashboard.putNumber("Right Slave Current: ", rTalonSlave.getOutputCurrent());
 	}
 	protected void initDefaultCommand() {
 		
-		
+		setDefaultCommand(new DriveWithControllerSimple());
 	}
 	public void DriveSimple(double leftDirection, double rightDirection){
 		lTalonMaster.set(leftDirection);
-		lTalonSlave.set(leftDirection);
-		
 		rTalonMaster.set(rightDirection);
-		rTalonSlave.set(rightDirection);
 	}
 }
 
