6ec651e72869e934f92f0f7eb506ae6e50ff1c29
tjrobotics <tjrobotics@tjrobotics-TEST>
2016-02-11 19:20:29 -0500

    Changed the way that buffer points are processed

--- 2016-Robot/src/org/usfirst/frc/team88/robot/MotionProfile.java
+++ 2016-Robot/src/org/usfirst/frc/team88/robot/MotionProfile.java
@@ -1,14 +1,13 @@
 package org.usfirst.frc.team88.robot;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Notifier;
 
 public class MotionProfile {
 	
 	CANTalon.MotionProfileStatus status = new CANTalon.MotionProfileStatus();
 	static int numPoints = 0;
+	private CANTalon _talon;
 	
 	public static double[][] Points = new double[numPoints][3];
 	
@@ -18,12 +17,17 @@ public class MotionProfile {
 	 * [0][2] = first point, duration
 	 * [1][0] = second point, position
 	 */
+	class PeriodicRunnable implements java.lang.Runnable {
+	    public void run() {  _talon.processMotionProfileBuffer();    }
+	}
+	Notifier notifier = new Notifier(new PeriodicRunnable());
 	
 	public void sendPoints (double[][] profile, CANTalon talon){
-		Timer timer = new Timer();
 		CANTalon.TrajectoryPoint point = new CANTalon.TrajectoryPoint();
 		point.isLastPoint = false;
 		point.zeroPos = false;
+		
+		notifier.startPeriodic(0.005);
 		
 		talon.getMotionProfileStatus(status);
 		
@@ -60,19 +64,7 @@ public class MotionProfile {
 		//I'll have to figure this out
 		
 	}
-	
-	public void resetTalon(CANTalon talon){
-		talon.clearMotionProfileTrajectories();
-	}
-	
-	class CheckBuffer extends TimerTask(CANTalon talon){
-		talon.processMotionProfileBuffer();
-	}
-	@Override
-	public void run() {
-		talon.processMotionProfileBuffer();
-	}
-}
+
 }
 
 
