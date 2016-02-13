3979fc2660046e84749f66a29c2cf9ec191c790b
tjrobotics <tjrobotics@tjrobotics-TEST>
2016-02-13 15:56:49 -0500

    State function for the intake in command

--- 2016-Robot/src/org/usfirst/frc/team88/robot/commands/IntakeIn.java
+++ 2016-Robot/src/org/usfirst/frc/team88/robot/commands/IntakeIn.java
@@ -10,24 +10,45 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class IntakeIn extends Command {
 	
+	private final static int BALL_IS_NOT_IN_NEST = 0;
+	private final static int BALL_IS_IN_NEST = 1;
+	private final static int BALL_HAS_PASSED_NEST = 2;
+	
+	private int state = BALL_IS_NOT_IN_NEST;
+	
     public IntakeIn() {
     	requires(Robot.intake);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	if (!Robot.intake.isBoulderInNest()) {
-        	Robot.intake.move(Intake.INTAKE_IN);
-    	}
+    	state = BALL_IS_NOT_IN_NEST;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	switch(state){
+    	case BALL_IS_NOT_IN_NEST:
+    		if(!Robot.intake.isBoulderInNest()){
+    			Robot.intake.move(Intake.INTAKE_IN);
+    		}
+    		state = BALL_IS_IN_NEST;
+    		break;
+    	case BALL_IS_IN_NEST:
+    		Robot.intake.move(Intake.INTAKE_IN/2);
+    		if(!Robot.intake.isBoulderInNest()){
+    			state = BALL_HAS_PASSED_NEST;
+    		}
+    		break;
+    	case BALL_HAS_PASSED_NEST:
+    		Robot.intake.move(0);
+    		break;
+    	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.intake.isBoulderInNest();
+        return state ==BALL_HAS_PASSED_NEST;
     }
 
     // Called once after isFinished returns true
