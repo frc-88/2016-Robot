2a50dce2bbc94539aaf2eb66d8fab63c62f140ad
tj2 <navtges@gmail.com>
2016-02-13 12:05:28 -0500

    Shooter prep state machine!

--- 2016-Robot/src/org/usfirst/frc/team88/robot/commands/PrepareShooter.java
+++ 2016-Robot/src/org/usfirst/frc/team88/robot/commands/PrepareShooter.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team88.robot.commands;
 
 import org.usfirst.frc.team88.robot.Robot;
+import org.usfirst.frc.team88.robot.subsystems.Intake;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -9,7 +10,14 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class PrepareShooter extends Command {
 
-	private boolean isShooterStarted = false;
+	private final static int START = 0;
+	private final static int INTAKE_MOVING = 1;
+	private final static int BOULDER_IN_NEST = 2;
+	private final static int SHOOTER_MOVING = 3;
+	private final static int DONE = 0;
+	
+	private int state = START;
+	private boolean done = false;
 	
     public PrepareShooter() {
     	requires(Robot.intake);
@@ -17,24 +25,37 @@ public class PrepareShooter extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	state = START;
+    	done = false;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if(!Robot.intake.isBoulderInNest()){
-    		
+    	switch (state) {
+    	case START:
+    		Robot.intake.move(Intake.INTAKE_OUT / 2);
+    		state = INTAKE_MOVING;
+    		break;
+    	case INTAKE_MOVING:
+    		if(Robot.intake.isBoulderInNest()) {
+    			Robot.intake.move(0);
+    			state = BOULDER_IN_NEST;
+    		}
+    		break;
+    	case BOULDER_IN_NEST:
+    		Robot.intake.startShooter();
+        	state = SHOOTER_MOVING;
+        	break;
+    	case SHOOTER_MOVING:
+    		if(Robot.intake.isShooterReady()) {
+    			done = true;
+    		}
     	}
-    	else if(!isShooterStarted){
-    	Robot.intake.startShooter();
-    	isShooterStarted = true;
-    	}
-    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.intake.isShooterReady();
+        return done;
     }
 
     // Called once after isFinished returns true
