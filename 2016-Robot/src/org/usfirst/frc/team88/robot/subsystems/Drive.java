a9add76ddef5815a9238bf7909f41f3ea2396799
MrNick <navtges@gmail.com>
2016-02-16 09:37:25 -0500

    More drive cleanup...back to basics.

--- 2016-Robot/src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ 2016-Robot/src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team88.robot.subsystems;
 
+import org.usfirst.frc.team88.robot.Robot;
 import org.usfirst.frc.team88.robot.RobotMap;
 import org.usfirst.frc.team88.robot.commands.DriveWithControllerClosed;
 
@@ -8,46 +9,39 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- *	Author:
- *	Nick Avtges
+ * Author: Nick Avtges
  */
 public class Drive extends Subsystem {
-    
-	private final CANTalon lTalonMaster, lTalonSlave, rTalonMaster, rTalonSlave;
-	
-	private final static double MAX_SPEED = 600;
-	private final static double RIGHT_P = 1.5;
-	private final static double LEFT_P = 1.5;
-	private final static double I = 0.01; 
-	private final static double D = 0.02;
-	private final static double DEAD_ZONE = 0.2;
-	private double difference = 0.0;
-			
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
-	public Drive(){
-				
+	private final CANTalon lTalonMaster, lTalonSlave, rTalonMaster, rTalonSlave;
+
+	private final static double MAX_SPEED = 600;
+	private final static double P = 1.0;
+	private final static double I = 0.0;
+	private final static double D = 0.0;
+
+	public Drive() {
+
 		// left side drive controllers
 		lTalonMaster = new CANTalon(RobotMap.driveLeftMaster);
 		lTalonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-	    lTalonMaster.reverseSensor(true);
-		//lTalonMaster.reverseOutput(true);
+		lTalonMaster.reverseSensor(true);
+		// lTalonMaster.reverseOutput(true);
 		lTalonMaster.setPosition(0);
-		lTalonMaster.setPID(LEFT_P, I, D);
+		lTalonMaster.setPID(P, I, D);
 		lTalonMaster.changeControlMode(CANTalon.TalonControlMode.Speed);
 
 		lTalonSlave = new CANTalon(RobotMap.driveLeftSlave);
 		lTalonSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
 		lTalonSlave.set(lTalonMaster.getDeviceID());
-		
+
 		// right side drive controllers
 		rTalonMaster = new CANTalon(RobotMap.driveRightMaster);
 		rTalonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-	    rTalonMaster.reverseSensor(true);
-	    //rTalonMaster.reverseOutput(true);
+		rTalonMaster.reverseSensor(true);
+		// rTalonMaster.reverseOutput(true);
 		rTalonMaster.setPosition(0);
-		rTalonMaster.setPID(RIGHT_P, I, D);
+		rTalonMaster.setPID(P, I, D);
 		rTalonMaster.changeControlMode(CANTalon.TalonControlMode.Speed);
 
 		rTalonSlave = new CANTalon(RobotMap.driveRightSlave);
@@ -55,8 +49,8 @@ public class Drive extends Subsystem {
 		rTalonSlave.set(rTalonMaster.getDeviceID());
 
 	}
-	
-	private void updateSmartDashboard(double diff) {
+
+	private void updateSmartDashboard() {
 		SmartDashboard.putNumber("Left Encoder: ", lTalonMaster.getPosition());
 		SmartDashboard.putNumber("Left Master Voltage: ", lTalonMaster.getOutputVoltage());
 		SmartDashboard.putNumber("Left Master Current: ", lTalonMaster.getOutputCurrent());
@@ -70,31 +64,22 @@ public class Drive extends Subsystem {
 		SmartDashboard.putNumber("Right Master Speed: ", -rTalonMaster.getSpeed());
 		SmartDashboard.putNumber("Right Slave Voltage: ", rTalonSlave.getOutputVoltage());
 		SmartDashboard.putNumber("Right Slave Current: ", rTalonSlave.getOutputCurrent());
-		
-		//Positive distance means favoring left side.  negative means favoring right side.
-		SmartDashboard.putNumber("Difference in Speed: ", diff);
 	}
-	
+
 	protected void initDefaultCommand() {
 		setDefaultCommand(new DriveWithControllerClosed());
 	}
-	
-	public void setSpeed (double leftDirection, double rightDirection){
+
+	public void setSpeed(double leftDirection, double rightDirection) {
 		SmartDashboard.putNumber("Left Input: ", leftDirection);
 		SmartDashboard.putNumber("Right Input: ", rightDirection);
-		
-		if(Math.abs(leftDirection) <= DEAD_ZONE){
-			leftDirection = 0;
-		}
-		if(Math.abs(rightDirection) <= DEAD_ZONE){
-			rightDirection = 0;
-		}
-		
+
+		leftDirection = Robot.oi.applyDeadZone(leftDirection);
+		rightDirection = Robot.oi.applyDeadZone(rightDirection);
+
 		lTalonMaster.set(-leftDirection * MAX_SPEED);
 		rTalonMaster.set(rightDirection * MAX_SPEED);
-		
-		difference = (Math.abs(lTalonMaster.getSpeed())-Math.abs(rTalonMaster.getSpeed()));
-		updateSmartDashboard(difference);
+
+		updateSmartDashboard();
 	}
 }
-
